205d51f8e9207ee40886ae8b2b4627d3ab411ad1
KleineFynn <40142390+kleinefynn@users.noreply.github.com>
2020-07-09 19:57:10 +0200

    Add files via upload

---
+++ example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using NomNoml.Languages;
+using NomNoml.Parser;
+
+namespace NomNoml
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input =
+                UmlFileIO.LoadUmlFromFile("stuff.txt");
+
+            
+            var classes = NomNomlLanguageParser.Parse<CSharpLanguage>(input);
+
+            UmlFileIO.WriteToFile("out", classes);
+        }
+    }
+}
---
+++ example/UmlFileIO.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NomNoml.Languages;
+
+namespace NomNoml
+{
+    public class UmlFileIO
+    {
+        public static string LoadUmlFromFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return null;
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+                return sr.ReadToEnd();
+        }
+
+        public static bool WriteToFile(string outputFolder, IEnumerable<Class> classes)
+        {
+            string backupFolder = outputFolder + "backup";
+            if (Directory.Exists(outputFolder))
+            {
+                if(Directory.Exists(backupFolder))
+                    Directory.Delete(backupFolder, true);
+                
+                Directory.Move(
+                    outputFolder,
+                    backupFolder
+                    );
+            }
+            Directory.CreateDirectory(outputFolder);
+            
+
+            foreach (var c in classes)
+            {
+                using (StreamWriter sw = File.CreateText(
+                    $"{outputFolder}/{c.Name}.cs")
+                )
+                    sw.Write(c.ToCode());
+            }
+            return false;
+        }
+    }
+}
